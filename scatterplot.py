7edf67e8d72399e8539bf6a6818a53ca8b2bb1df
r3card0 <ricardo.rdz@pm.me>
2022-09-24 21:18:37 -0500

    Added description of the program

---
+++ scatterplot.py
@@ -0,0 +1,19 @@
+import pandas as pd
+from openpyxl.workbook import workbook
+import matplotlib.pyplot as plt
+import seaborn as np
+from df_copy import copy
+
+def description():
+    programa_description= """
+    This program creates a scatter plot
+    """
+    return programa_description
+
+df = copy()
+
+def run():
+    pass
+
+if __name__ == "__main__":
+    run()
